7b1a75731f860d6df1a18ac6df9cadcc4cc4c8c7
fikrialwan <fikriar93@gmail.com>
2024-02-24 16:53:54 +0700

    feat: add jakarta plus font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { cn } from "~/lib/utils";
+import { fontJakarta } from "~/styles/font";
 import "~/styles/globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Coffeeshopedia",
@@ -16,7 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn(fontJakarta.variable, "font-jakarta")}>
+        {children}
+      </body>
     </html>
   );
 }
