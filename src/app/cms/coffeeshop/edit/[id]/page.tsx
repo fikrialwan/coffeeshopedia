c6cb5451c0afebb11c6fac31c2f4aa3ac8b1462e
fikrialwan <fikriar93@gmail.com>
2024-02-29 23:55:42 +0700

    feat: add iniital styling edit page

--- src/app/cms/coffeeshop/edit/[id]/page.tsx
+++ src/app/cms/coffeeshop/edit/[id]/page.tsx
@@ -1,9 +1,26 @@
+"use client"
+
 import React from "react"
+import { ListCoffeeshop } from "~/lib/components/cms/coffeeshop/column"
+import CMSCoffeeshopEdit from "~/lib/components/pages/cms-coffeeshop-edit"
 
 export default function CoffeeshopEditPage({
   params: { id },
 }: {
   params: { id: string }
 }) {
-  return <div>CoffeeshopEditPage {id}</div>
+  const defaultValueCoffeeshop = ListCoffeeshop.find(
+    (coffeeshop) => coffeeshop?.id === id
+  )
+
+  return (
+    <CMSCoffeeshopEdit
+      id={defaultValueCoffeeshop?.id || id}
+      address={defaultValueCoffeeshop?.address || ""}
+      description={defaultValueCoffeeshop?.description || ""}
+      facility={defaultValueCoffeeshop?.facility || ""}
+      name={defaultValueCoffeeshop?.name || ""}
+      urlMaps={defaultValueCoffeeshop?.description || ""}
+    />
+  )
 }
